35327e09a37eb6c1c761e69c9b2f6da5ad857fcc
Jue Yang <jueyang22@gmail.com>
2012-04-12 14:40:34 -0400

    changed ie7 css and adding ieresize to site.js

--- js/site.js
+++ js/site.js
@@ -110,6 +110,7 @@ $(function() {
 	    $('#sidebar ul.mainmenu li.third a').css('width','185px');
 	    $('#sidebar ul.mainmenu li.second a').css('width','251px');
 	    $('#attribution').css('left','650px');
+	    $('textarea.embed-code, textarea.api-code').addClass('ie-resize');
 	    layer = 'fews-net.sahel-fewsnet-food-security-aprjune-2012';
 	    $('.datalist a#fews-net\\.sahel-fewsnet-food-security-aprjune-2012').addClass('active');
 	    $('.datalist a#fews-net\\.sahel-fewsnet-food-security-aprjune-2012').parent().parent().prependTo('#sortable');
@@ -132,6 +133,7 @@ $(function() {
 	    $('#sidebar ul.mainmenu li.third a').css('width','120px');
 	    $('#sidebar ul.mainmenu li.second a').css('width','156px');
 	    $('#attribution').css('left','425px');
+	    $('textarea.embed-code, textarea.api-code').removeClass('ie-resize');
 	  }
 	});
 	
